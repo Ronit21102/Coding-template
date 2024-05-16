68462a4d8dd2ee1e786efa6f741bb642d0e295d8
Ronit21102 <60790772+Ronit21102@users.noreply.github.com>
2024-05-17 00:43:04 +0530

    Update DSU.cpp

--- Graph/DSU.cpp
+++ Graph/DSU.cpp
@@ -9,7 +9,7 @@ int find(int i)
     return parent[i];
 }
 
-void Union(int x, int y)
+void UnionR(int x, int y)
 {
     int p_x = find(x);
     int p_y = find(y);
@@ -31,3 +31,22 @@ void Union(int x, int y)
         }
     }
 }
+
+ void UnionS(int x, int y) {
+	    int x_parent = find(x);
+	    int y_parent = find(y);
+	
+	    if (x_parent == y_parent) 
+	        return;
+	
+	    if(size[x_parent] > size[y_parent]) {
+	        parent[y_parent] = x_parent;
+	        size[x_parent]  += size[y_parent];
+	    } else if(size[x_parent] < size[y_parent]) {
+	        parent[x_parent] = y_parent;
+	        size[y_parent]  += size[x_parent];
+	    } else {
+	        parent[x_parent] = y_parent;
+	        size[y_parent]  += size[x_parent];
+	    }
+	}
